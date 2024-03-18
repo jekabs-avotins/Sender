d6253e2106922c70602c4e21dfca1237dbc242c7
Jēkabs Avotiņš <jekabs.avotins@edu.rtu.lv>
2024-03-18 02:58:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(11000);
+
+            string receiverIP = "127.0.0.1";
+            int receiverPort = 11001;
+
+            Console.WriteLine("Enter text to send. Type 'exit' to close.");
+
+            while (true)
+            {
+                string message = Console.ReadLine()!;
+                byte[] bytesToSend = Encoding.ASCII.GetBytes(message);
+                udpClient.Send(bytesToSend, bytesToSend.Length, receiverIP, receiverPort);
+                
+
+                if (message.ToLower() == "exit")
+                {
+                    Console.WriteLine("Sender closed.");
+                    break;
+                }
+            }
+            
+        }
+    }
+}
